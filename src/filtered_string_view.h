a01e01198600068aaa0e91921b5d1ea4047dd5d0
Hayden Smith <hayden.smith@unsw.edu.au>
2024-06-15 21:41:22 +1000

    Ready for 24T2

---
+++ src/filtered_string_view.h
@@ -0,0 +1,38 @@
+#ifndef COMP6771_ASS2_FSV_H
+#define COMP6771_ASS2_FSV_H
+
+#include <compare>
+#include <functional>
+#include <iterator>
+#include <optional>
+#include <string>
+
+namespace fsv {
+	class filtered_string_view {
+		class iter {
+		 public:
+			using MEMBER_TYPEDEFS_GO_HERE = void;
+
+			iter();
+
+			auto operator*() const -> void; // change this
+			auto operator->() const -> void; // change this
+
+			auto operator++() -> iter&;
+			auto operator++(int) -> iter;
+			auto operator--() -> iter&;
+			auto operator--(int) -> iter;
+
+			friend auto operator==(const iter&, const iter&) -> bool;
+			friend auto operator!=(const iter&, const iter&) -> bool;
+
+		 private:
+			/* Implementation-specific private members */
+		};
+
+	 public:
+	 private:
+	};
+} // namespace fsv
+
+#endif // COMP6771_ASS2_FSV_H
